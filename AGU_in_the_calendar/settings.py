32606f41c7eddf697bbb952770e8bc46441a80f6
Evgeny <evgeny.lavrov2002@mail.ru>
2023-04-06 16:26:04 +0700

    Сгенерирован новый SECRET_KEY и перенесен в виртуальное окружение. Установлен русский язык и часовой пояс +7

--- AGU_in_the_calendar/settings.py
+++ AGU_in_the_calendar/settings.py
@@ -9,18 +9,24 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+DOTENV_PATH = os.path.join(BASE_DIR, "vars.env")
+if os.path.exists(DOTENV_PATH):
+    load_dotenv(DOTENV_PATH)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k10%ry-_lq96#@i-s56!5(w@0f-_t$0a69^f&bj#nkkfm09y@h'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -104,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Novosibirsk'
 
 USE_I18N = True
 
