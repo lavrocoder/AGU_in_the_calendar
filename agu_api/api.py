86a3c1c8a5608bbb5c42ecfecf28330c7bd617b3
Evgeny <evgeny.lavrov2002@mail.ru>
2023-04-15 14:08:10 +0700

    Добавлены Api для гугул календаря и API для Агу

--- agu_api/api.py
+++ agu_api/api.py
@@ -1,2 +1,28 @@
+import requests
+
+
 class AguApi:
-    pass
+    def __init__(self):
+        self.api = 'https://rb.asu.ru/api'
+
+    def method(self, name: object, **kwargs: object) -> object:
+        params = kwargs
+        response = requests.get(f"{self.api}/{name}", params=params)
+        return response.json()
+
+    def timetable(self, sday=None, time_interval=None, type=None, value=None):
+        params = {'sday': sday, 'time_interval': time_interval, 'type': type, 'value': value}
+        return self.method('timetable', **params)
+
+    def groups(self):
+        return self.method('groups')
+
+    def timetableBG(self, sday=None, time_interval=None, type=None, value=None):
+        groups = self.groups()
+        groupID = None
+        for group in groups:
+            if group["SNAME"] == value:
+                groupID = group['ID']
+                break
+
+        return self.timetable(sday, time_interval, type, groupID)
--- calendar_api/api.py
+++ calendar_api/api.py
@@ -1,2 +1,55 @@
-class CalendarApi:
-    pass
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+
+
+class GoogleCalendar:
+    SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+    def __init__(self, file_path: str):
+        credentials = service_account.Credentials.from_service_account_file(
+            filename=file_path, scopes=self.SCOPES
+        )
+        self.service = build('calendar', 'v3', credentials=credentials)
+
+    def get_calendar_list(self):
+        return self.service.calendarList().list().execute()
+
+    def add_calendar(self, name):
+        new_calendar = {
+            'summary': name,
+            'timeZone': 'Asia/Novosibirsk'
+        }
+        return self.service.calendars().insert(body=new_calendar).execute()
+
+    def add_event(self, calendar_id, event):
+        return self.service.events().insert(
+            calendarId=calendar_id,
+            body=event).execute()
+
+    def delete_event(self, calendar_id, event_id):
+        return self.service.events().delete(
+            calendarId=calendar_id,
+            eventId=event_id).execute()
+
+    def add_rule(self, calendar_id):
+        rule = {
+            'scope': {
+                'type': 'default',
+            },
+            'role': 'reader'
+        }
+        return self.service.acl().insert(calendarId=calendar_id, body=rule).execute()
+
+    def create_calendar(self, name):
+        calendar = self.add_calendar(name)
+        calendar_id = calendar['id']
+        self.add_rule(calendar_id)
+        calendar_link = f'https://calendar.google.com/calendar/r?cid={calendar_id}'
+        obj = {
+            'id': calendar_id,
+            'url': calendar_link,
+        }
+        return obj
+
+    def get_colors(self):
+        return self.service.colors().get().execute()
