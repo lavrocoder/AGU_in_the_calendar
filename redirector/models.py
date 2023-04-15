e167184377b323c16cf8149449e83684dd29ae6b
Evgeny <evgeny.lavrov2002@mail.ru>
2023-04-15 14:07:21 +0700

    Добавлены модели базы данных

--- redirector/models.py
+++ redirector/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Schedule(models.Model):
+    agu_id = models.CharField(max_length=250)
+    discipline_name = models.CharField(max_length=250)
+    type = models.CharField(max_length=250)
+    distant = models.BooleanField(default=False)
+    place = models.CharField(max_length=250)
+    time_start = models.TimeField()
+    time_end = models.TimeField()
+    teacher = models.CharField(max_length=250)
+
+
+class GroupCalendar(models.Model):
+    google_id = models.CharField(max_length=250)
+    url = models.CharField(max_length=250)
+    agu_id = models.CharField(max_length=250)
+    group = models.CharField(max_length=250)
